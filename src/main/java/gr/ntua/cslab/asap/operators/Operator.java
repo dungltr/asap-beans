b7826fb9dc4f4aa0c0a8161c3228142d3087d97a
dungltr <dung80@gmail.com>
2018-04-27 16:34:24 +0200

    add max_line in operators

--- src/main/java/gr/ntua/cslab/asap/operators/Operator.java
+++ src/main/java/gr/ntua/cslab/asap/operators/Operator.java
@@ -125,7 +125,7 @@ public class Operator {
                         //this.initializeDatasource();
                         //outPoints = dataSource.getOutputSpacePoints(e.getKey());
                         CSVFileManager file = new CSVFileManager();
-                        file.setFilename(directory + "/data/" + e.getKey() + "_realValue.csv");
+                        file.setFilename(directory + "/data/" + e.getKey() + ".csv");
                         //////Dung edit to put DREAM in this step
                         //outPoints = dream.runDream(Library,directory,e.getKey());
                         /*
